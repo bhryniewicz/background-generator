03cb4e62bf0a2b451d088a952298ba9da1ec2cff
Bartosz Hryniewicz <bartosz.hryniewicz3@gmail.com>
2025-04-09 10:01:08 +0200

    remove redundatnt dependency

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="max-h-min col-start-3 col-end-11 flex items-center justify-between font-semibold py-6 px-10 bg-white/40 text-white rounded-xl border-2 border-white mt-8">
+    <nav className="text-sm max-h-min col-start-3 col-end-11 flex items-center justify-between font-semibold py-4 px-8 bg-white/40 text-white rounded-xl border-2 border-white mt-8">
       <Link href="/">
         <div className="flex items-center gap-4">
           {/* <Image
@@ -20,17 +20,17 @@ export const Navbar = () => {
         height={40}
         placeholder="blur"
       /> */}
-          <h1 className="text-md">BGenerator</h1>
+          <h1>BGenerator</h1>
         </div>
       </Link>
-      <ul className="flex gap-16 text-md">
+      <ul className="flex gap-16 ">
         {routes.map(({ label, route }) => {
           const isActive = pathname === route;
 
           return (
             <Link href={route} key={route}>
               <li
-                className={`relative before:content-[''] before:absolute before:-bottom-[3px] before:left-0 before:w-full before:h-[3px] before:transition-opacity before:duration-300 ${
+                className={`relative before:content-[''] before:absolute before:-bottom-[3px] before:left-0 before:w-full before:h-[2px] before:transition-opacity before:duration-300 ${
                   isActive
                     ? "before:opacity-100 before:bg-white"
                     : "before:opacity-0 hover:before:opacity-100 before:bg-white"
