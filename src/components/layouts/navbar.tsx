502088a224c2432b09b05f59a2f505e52dd62c79
Bartosz Hryniewicz <bartosz.hryniewicz3@gmail.com>
2025-04-30 14:30:00 +0200

    update navbar stlying

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -5,12 +5,11 @@ import { usePathname } from "next/navigation";
 import { routes } from "@/config/paths";
 import { Button } from "../ui/button";
 import { useState } from "react";
-import { CrossIcon, MenuIcon } from "lucide-react";
+import { MenuIcon } from "lucide-react";
 
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -21,16 +20,16 @@ export const Navbar = () => {
   const pathname = usePathname();
 
   const mappedRoutes = () => (
-    <>
+    <ul className="flex flex-col md:flex-row justify-start gap-10 flex-grow-1 w-full">
       {routes.map(({ label, route }) => {
         const isActive = pathname === route;
         return (
           <Link href={route} key={route} prefetch>
             <li
-              className={`text-lg font-semibold relative before:content-[''] before:absolute before:-bottom-[3px] before:left-0 before:w-full before:h-[2px] before:transition-opacity before:duration-300 ${
+              className={` hover:text-white text-xl md:text-base font-semibold relative before:content-[''] before:absolute before:-bottom-[3px] before:left-0 before:w-full before:h-[2px] before:transition-opacity before:duration-300 ${
                 isActive
-                  ? "before:opacity-100 before:bg-white"
-                  : "before:opacity-0 hover:before:opacity-100 before:bg-white"
+                  ? "before:opacity-100 before:bg-white text-white"
+                  : "before:opacity-0 hover:before:opacity-100 before:bg-white text-white/70"
               }`}
             >
               {label}
@@ -38,17 +37,17 @@ export const Navbar = () => {
           </Link>
         );
       })}
-      <Button className="w-full md:w-auto mt-auto">Sign in</Button>
-    </>
+      <Button className="w-full md:w-auto mt-auto max-md:mb-16">Sign in</Button>
+    </ul>
   );
 
   return (
-    <div className="pointer-events-none text-base col-span-full 2xl:col-span-8 2xl:col-start-3 flex items-center justify-between font-semibold md:px-8 md:bg-white/40 text-white rounded-xl md:border-2 md:border-white pt-8 md:mt-8 z-100">
+    <div className="pointer-events-none text-base col-span-full 2xl:col-span-12 2xl:col-start-1 flex items-center justify-between font-semibold md:px-8 text-white pt-8 z-100">
       <div className="flex items-center justify-between w-full pointer-events-auto">
         <Link href="/" className="flex items-center gap-4">
-          <h1>BGenerator</h1>
+          <h1 className="font-semibold text-3xl">PicMorph</h1>
         </Link>
-        <ul className="hidden md:flex items-center gap-12">{mappedRoutes()}</ul>
+        <div className="hidden md:block">{mappedRoutes()}</div>
 
         {/* mobile */}
         <div className="block md:hidden">
@@ -59,7 +58,7 @@ export const Navbar = () => {
             {isMenuOpen && (
               <>
                 <DialogTitle className="text-white "></DialogTitle>
-                <DialogContent className="flex flex-col gap-6 items-start justify-center bg-[#131313] h-screen z-50 w-full pointer-events-auto rounded-none text-white p-8 list-none border-0">
+                <DialogContent className="flex flex-col gap-6 items-start bg-[#131313] h-screen z-50 w-full pointer-events-auto rounded-none text-white p-8 list-none border-0">
                   <DialogHeader className="mb-24 text-3xl font-semibold">
                     PicMorph
                   </DialogHeader>
